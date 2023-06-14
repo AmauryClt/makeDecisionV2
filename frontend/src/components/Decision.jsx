3eeca79b69fb16b0ea5f78941466218bca2db8f2
Fabien Chbd <fabien.chabaud1@gmail.com>
2023-06-14 16:38:43 +0200

    Formualaire V2 - CSS Ok mobile et desktop (Correctif)

--- frontend/src/components/Decision.jsx
+++ frontend/src/components/Decision.jsx
@@ -1,16 +1,9 @@
-import styles from "./decision.module.scss";
 import React, { useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
+import styles from "./decision.module.scss";
 
 export default function Decision() {
-  console.log(Editor);
-
   const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
 
   return (
     <div>
@@ -22,43 +15,130 @@ export default function Decision() {
         <label className={styles.title}>
           <input type="text" name="title" placeholder="Titre de ta décision" />
         </label>
-        <label>
-          <div className={styles.editor}>
-            <Editor
-              onInit={(evt, editor) => (editorRef.current = editor)}
-              initialValue="<p>Donnez nous des détails sur votre idée !!!</p>"
-              init={{
-                height: 300,
-                menubar: true,
-              }}
-            />
-          </div>
-        </label>
-        <label>
-          <div className={styles.editor}>
-            <Editor
-              onInit={(evt, editor) => (editorRef.current = editor)}
-              initialValue="<p>Quel en seront les bénéfices ?</p>"
-              init={{
-                height: 300,
-                menubar: true,
-              }}
-            />
-          </div>
-        </label>
-        <label>
-          Risques :
-          <div className={styles.editor}>
-            <Editor
-              onInit={(evt, editor) => (editorRef.current = editor)}
-              initialValue="<p>Et les risques ?</p>"
-              init={{
-                height: 300,
-                menubar: true,
-              }}
-            />
-          </div>
-        </label>
+        <div className={styles.editor}>
+          <Editor
+            onInit={(evt, editor) => {
+              editorRef.current = editor;
+            }}
+            initialValue="<p>Donnez nous des détails sur votre idée !!!</p>"
+            init={{
+              plugins: "lists",
+              height: 300,
+              toolbar: [
+                { name: "history", items: ["undo", "redo"] },
+                {
+                  name: "formatting",
+                  items: [
+                    "bold",
+                    "italic",
+                    "forecolor",
+                    "backcolor",
+                    "fontsizeinput",
+                  ],
+                },
+                {
+                  name: "alignment",
+                  items: [
+                    "alignleft",
+                    "aligncenter",
+                    "alignright",
+                    "alignjustify",
+                  ],
+                },
+
+                {
+                  name: "indentation",
+                  items: ["bullist", "numlist", "outdent", "indent"],
+                },
+              ],
+              color_cols: 5,
+              menubar: true,
+            }}
+          />
+        </div>
+        <div className={styles.editor}>
+          <Editor
+            onInit={(evt, editor) => {
+              editorRef.current = editor;
+            }}
+            initialValue="<p>Quel en seront les bénéfices ?</p>"
+            init={{
+              plugins: "lists",
+              height: 300,
+              toolbar: [
+                { name: "history", items: ["undo", "redo"] },
+                {
+                  name: "formatting",
+                  items: [
+                    "bold",
+                    "italic",
+                    "forecolor",
+                    "backcolor",
+                    "fontsizeinput",
+                  ],
+                },
+                {
+                  name: "alignment",
+                  items: [
+                    "alignleft",
+                    "aligncenter",
+                    "alignright",
+                    "alignjustify",
+                  ],
+                },
+
+                {
+                  name: "indentation",
+                  items: ["bullist", "numlist", "outdent", "indent"],
+                },
+              ],
+              color_cols: 5,
+              menubar: true,
+            }}
+          />
+        </div>
+        Risques :
+        <div className={styles.editor}>
+          <Editor
+            onInit={(evt, editor) => {
+              editorRef.current = editor;
+            }}
+            initialValue="<p>Et les risques ?</p>"
+            init={{
+              plugins: "lists",
+              height: 300,
+              toolbar: [
+                { name: "history", items: ["undo", "redo"] },
+                {
+                  name: "formatting",
+                  items: [
+                    "bold",
+                    "italic",
+                    "forecolor",
+                    "backcolor",
+                    "fontsizeinput",
+                  ],
+                },
+                {
+                  name: "alignment",
+                  items: [
+                    "alignleft",
+                    "aligncenter",
+                    "alignright",
+                    "alignjustify",
+                  ],
+                },
+
+                {
+                  name: "indentation",
+                  items: ["bullist", "numlist", "outdent", "indent"],
+                },
+              ],
+              color_cols: 5,
+              menubar: true,
+            }}
+          />
+        </div>
         <label>
           Service(s) impactés
           <em>Utilisez la touche ctrl pour une sélection multiple</em>
