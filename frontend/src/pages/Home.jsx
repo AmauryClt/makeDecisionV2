5be13bca8056a3e0c035d8a1b5b7f726025571ae
ThomasD26 <thomas@ThomasD>
2023-06-14 14:25:17 +0200

    com

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,12 @@ import styles from "./home.module.scss";
 export default function Home() {
   return (
     <main>
-        <div className={styles.creer}/>
-        <div className={styles.attente}/>
-        <div className={styles.désaccord}/>
-        <div className={styles.validée}/>
-        <div className={styles.enplace}/>
-        <div className={styles.archivée}/>
+      <div className={styles.creer} />
+      <div className={styles.attente} />
+      <div className={styles.désaccord} />
+      <div className={styles.validée} />
+      <div className={styles.enplace} />
+      <div className={styles.archivée} />
     </main>
   );
 }
