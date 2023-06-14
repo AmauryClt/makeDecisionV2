471f4f47bbc8758631781fbaab3c0cbd1440fb21
ThomasD26 <thomas@ThomasD>
2023-06-14 14:16:32 +0200

    Début de la page Home pas fini

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,14 @@
-import Counter from "../components/Counter";
-import logo from "../assets/logo.svg";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <main>
+        <div className={styles.creer}/>
+        <div className={styles.attente}/>
+        <div className={styles.désaccord}/>
+        <div className={styles.validée}/>
+        <div className={styles.enplace}/>
+        <div className={styles.archivée}/>
+    </main>
   );
 }
