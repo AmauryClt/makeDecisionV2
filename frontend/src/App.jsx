472bdd4e40988fe6c93cb9201e6bcb89f2f21554
alexdubrulle <alexdubrullefagnoni26@hotmail.fr>
2023-06-21 11:33:45 +0200

    navbar avec routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import VotePage from "./components/VotePage";
-import DisagreementPage from "./components/DisagreementPage";
 import ValidPage from "./components/ValidPage";
-import EstablishmentPage from "./components/EstablishmentPage";
-import ArchivePage from "./components/ArchivePage";
 import Footer from "./components/Footer";
 
 import "./App.css";
@@ -18,12 +15,8 @@ function App() {
         <Routes>
           <Route path="*" element={<Home />} />
           <Route path="/VotePage" element={<VotePage />} />
-          <Route path="/DisagreementPage" element={<DisagreementPage />} />
           <Route path="/ValidPage" element={<ValidPage />} />
-          <Route path="/EstablishmentPage" element={<EstablishmentPage />} />
-          <Route path="/ArchivePage" element={<ArchivePage />} />
         </Routes>
-        <p>coucou</p>
         <Footer />
       </div>
     </Router>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./navbar.module.scss";
 
 function Navbar() {
   return (
-    <nav className="nav-links">
-      <div className="Liens">
-        <Link className="créer" to="/créertadécision">
-          Créer ta décision
+    <nav className={styles.navlinks}>
+      <div className={styles.ul}>
+        <Link className={styles.créer} to="/créertadécision">
+          <li> Créer ta décision</li>
         </Link>
-        <Link className="en-cours" to="/encoursdevote">
-          En cours de vote
+        <Link className={styles.encours} to="/VotePage">
+          <li> En cours de vote</li>
         </Link>
-        <Link className="terminé" to="/votationterminé">
-          Votation terminé
+        <Link className={styles.termine} to="/ValidPage">
+          <li> Votation terminé</li>
         </Link>
       </div>
+      <style>
+        @import
+        url("https://fonts.googleapis.com/css2?family=Lato:wght@300;900&family=Permanent+Marker&family=Raleway:wght@900&display=swap");
+      </style>
     </nav>
   );
 }
