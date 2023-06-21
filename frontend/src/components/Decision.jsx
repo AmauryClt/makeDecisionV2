acf612f07b1f1728e0b3ee450c496bebd59d51ed
CLOT Amaury <amauryclt@gmail.com>
2023-06-21 15:19:53 +0200

    database crée

--- frontend/src/components/Decision.jsx
+++ frontend/src/components/Decision.jsx
@@ -17,7 +17,9 @@ export default function Decision() {
 
   return (
     <main>
-      <h1 className={styles.banniere}>Interface de création d'une demande de décision</h1>
+      <h1 className={styles.banniere}>
+        Interface de création d'une demande de décision
+      </h1>
       <p className={styles.intro}>
         Soyez le plus précis dans votre idée, les détails sont les bienvenus !!!
       </p>
