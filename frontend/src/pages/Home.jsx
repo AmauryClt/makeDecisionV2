db0c036cafab2ded76118883aa24242eff3d3bbc
ThomasD26 <thomas@ThomasD>
2023-06-21 11:21:25 +0200

    ajustement du css de la page Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,37 @@ import styles from "./home.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.mainHome}>
-      <div className={styles.creer} />
-      <div className={styles.attente} />
-      <div className={styles.désaccord} />
-      <div className={styles.validée} />
-      <div className={styles.enplace} />
-      <div className={styles.archivée} />
+    <main>
+      <h1 className={styles.banniere}> Make Desicion</h1>
+      <div className={styles.mainHome}>
+        <div className={styles.Creer}>
+          <img
+            className={styles.imgCards}
+            src="./src/assets/makesensecreer.jpg"
+            alt="Makesense.Logo"
+          />
+          <h2>Créer ta décision</h2>
+          <p>Coucou</p>
+        </div>
+        <div className={styles.EnCours}>
+          <img
+            className={styles.imgCards}
+            src="./src/assets/encoursdevote.jpeg"
+            alt="Makesense.Logo"
+          />
+          <h2>En cours de vote</h2>
+          <p>Coucou</p>
+        </div>
+        <div className={styles.VoteEnd}>
+          <img
+            className={styles.imgCards}
+            src="./src/assets/makesensevalide.jpg"
+            alt="Makesense.Logo"
+          />
+          <h2>Votation terminé</h2>
+          <p>Coucou</p>
+        </div>
+      </div>
     </main>
   );
 }
