f6ddda3a2512b44398f6ae93b8c3134ab974c05b
FabienChbd <117373089+FabienChbd@users.noreply.github.com>
2023-06-22 11:11:43 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Decision from "./components/Decision";
 import CreatePage from "./components/CreatePage";
-import Home from "./pages/Home";
 import VotePage from "./components/VotePage";
 import ValidPage from "./components/ValidPage";
 import Footer from "./components/Footer";
@@ -17,7 +15,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<LoginForm />} />
-          <Route path="/CreatePage" element={<Decision />} />
+          <Route path="/CreatePage" element={<CreatePage />} />
           <Route path="/VotePage" element={<VotePage />} />
           <Route path="/ValidPage" element={<ValidPage />} />
           <Route path="/Profil" element={<Profil />} />
