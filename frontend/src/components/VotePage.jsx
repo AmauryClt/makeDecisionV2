978095a054adf422eae30e46114f8b01dc2d331c
alexdubrulle <alexdubrullefagnoni26@hotmail.fr>
2023-07-05 15:09:03 +0200

    popup avec block 1 a 5

--- frontend/src/components/VotePage.jsx
+++ frontend/src/components/VotePage.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState, useEffect } from "react";
 import styles from "./votePage.module.scss";
 
 export default function VotePage() {
   const [demands, setDemands] = useState([]);
+  const [selectedDemand, setSelectedDemand] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:5001/demand")
@@ -15,12 +17,21 @@ export default function VotePage() {
       });
   }, []);
 
+  const openPopup = (demand) => {
+    setSelectedDemand(demand);
+  };
+
+  const closePopup = () => {
+    setSelectedDemand(null);
+  };
+
   return (
     <main>
       <h1 className={styles.banniere}>Décision en attente de vote</h1>
       <div className={styles.dataContainer}>
         {demands.map((demand) => (
-          <div key={demand.Id}>
+          // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+          <div key={demand.Id} onClick={() => openPopup(demand)}>
             <h3>{demand.Title}</h3>
             <p>{demand.Statut}</p>
           </div>
@@ -34,6 +45,44 @@ export default function VotePage() {
           <p>Lorem ipsum</p>
         </div>
       </div>
+      {selectedDemand && (
+        <div className={`${styles.popup} ${styles.popupContainer}`}>
+          <div className={styles.popupContent}>
+            <button
+              className={styles.popupButton}
+              type="button"
+              onClick={closePopup}
+            >
+              X
+            </button>
+            <div className={styles.block1}>
+              <div className={styles.block2}>
+                <div className={styles.block3}>
+                  <h3 className={styles.title}>{selectedDemand.Title}</h3>
+                  <p>{selectedDemand.user_Id}</p>
+                </div>
+                <div className={styles.block4}>
+                  <p>{selectedDemand.Content}</p>
+                  <p>{selectedDemand.Utility}</p>
+                  <p>{selectedDemand.Context}</p>
+                  <p>{selectedDemand.Benefice}</p>
+                  <p>{selectedDemand.Inconvenience}</p>
+                  <p>{selectedDemand.Complement}</p>
+                </div>
+              </div>
+              <div className={styles.block5}>
+                <p>{selectedDemand.Deadline}</p>
+                <p>{selectedDemand.Note}</p>
+                <p>{selectedDemand.Statut}</p>
+                <p>Avancement des votes</p>
+                <p>Salarié Votant</p>
+                <p>Expert Votant</p>
+                <p>Service impacté</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
