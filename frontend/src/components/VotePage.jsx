e883c6fdbdcf7eb2a4fa382946788484739b26e5
CLOT Amaury <amauryclt@gmail.com>
2023-07-06 14:07:52 +0200

    US popup WIP¨amaury pour RP dev

--- frontend/src/components/VotePage.jsx
+++ frontend/src/components/VotePage.jsx
@@ -68,41 +68,6 @@ export default function VotePage() {
                   <p>{selectedDemand.Benefice}</p>
                   <p>{selectedDemand.Inconvenience}</p>
                   <p>{selectedDemand.Complement}</p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Hendrerit gravida rutrum quisque non tellus orci ac
-                    auctor augue. Risus sed vulputate odio ut enim blandit
-                    volutpat maecenas volutpat. Dictum fusce ut placerat orci
-                    nulla pellentesque. Ac ut consequat semper viverra nam
-                    libero. Hac habitasse platea dictumst quisque sagittis purus
-                    sit amet. Porttitor rhoncus dolor purus non enim praesent
-                    elementum facilisis leo. Arcu non sodales neque sodales ut
-                    etiam. Euismod elementum nisi quis eleifend quam adipiscing
-                    vitae proin. Malesuada fames ac turpis egestas sed. Viverra
-                    mauris in aliquam sem fringilla ut morbi tincidunt augue.
-                    Leo vel orci porta non pulvinar neque laoreet suspendisse
-                    interdum. Ullamcorper malesuada proin libero nunc consequat
-                    interdum varius sit. Sit amet tellus cras adipiscing.
-                    Ultrices gravida dictum fusce ut placerat orci. Ut porttitor
-                    leo a diam sollicitudin tempor id eu nisl. Massa tincidunt
-                    dui ut ornare lectus sit amet est placerat. Lectus
-                    vestibulum mattis ullamcorper velit. Amet justo donec enim
-                    diam vulputate ut pharetra sit. Pellentesque elit
-                    ullamcorper dignissim cras tincidunt lobortis feugiat. Risus
-                    pretium quam vulputate dignissim suspendisse. Nibh sit amet
-                    commodo nulla facilisi nullam vehicula. Nam at lectus urna
-                    duis convallis convallis tellus. Neque convallis a cras
-                    semper auctor neque. Viverra accumsan in nisl nisi
-                    scelerisque. Fermentum dui faucibus in ornare quam viverra
-                    orci. Sed viverra tellus in hac habitasse. Senectus et netus
-                    et malesuada fames ac. Posuere lorem ipsum dolor sit amet
-                    consectetur adipiscing elit. Etiam non quam lacus
-                    suspendisse faucibus interdum posuere lorem. Velit laoreet
-                    id donec ultrices tincidunt arcu non. Lorem ipsum dolor sit
-                    amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Hendrerit
-                  </p>
                 </div>
               </div>
               <div className={styles.block5}>
@@ -112,7 +77,7 @@ export default function VotePage() {
                 <p>Avancement des votes</p>
                 <p>Salarié Votant</p>
                 <p>Expert Votant</p>
-                <p>Service impacté</p>
+                <p>{selectedDemand.ServiceImpact}</p>
               </div>
             </div>
           </div>
