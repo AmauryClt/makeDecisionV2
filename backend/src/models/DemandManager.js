67a14dc0f755e9100876d086edf938fb5c43e9bb
Fabien Chbd <fabien.chabaud1@gmail.com>
2023-07-07 13:51:00 +0200

    Database

--- backend/src/models/DemandManager.js
+++ backend/src/models/DemandManager.js
@@ -5,29 +5,9 @@ class DemandManager extends AbstractManager {
     super({ table: "demand" });
   }
 
-  // add(demand) {
-  //   return this.database.query(
-  //     `insert into ${this.table}(Title,Deadline,Content,Utility,Context,Benefice,Inconvenience,Serviceimpact,Note)values (?,?,?,?,?,?,?,?,?)`,
-  //     [
-  //       demand.Title,
-  //       demand.Deadline,
-  //       demand.Content,
-  //       demand.Utility,
-  //       demand.Context,
-  //       demand.Benefice,
-  //       demand.Inconvenience,
-  //       demand.ServiceImpact,
-  //       demand.Note,
-  //     ]
-  //   );
-  // }
-
   add(demand) {
     return this.database.query(
-      `INSERT INTO demand (Title, Deadline, Content, Utility, Context, Benefice, Inconvenience, ServiceImpact, Note)
-SELECT ?, ?, ?, ?, ?, ?, ?, serv.Id, ?
-FROM serv
-INNER JOIN demand ON serv.Serv = ?`,
+      `insert into ${this.table}(Title,Deadline,Content,Utility,Context,Benefice,Inconvenience,Note)values (?,?,?,?,?,?,?,?)`,
       [
         demand.Title,
         demand.Deadline,
@@ -36,9 +16,7 @@ INNER JOIN demand ON serv.Serv = ?`,
         demand.Context,
         demand.Benefice,
         demand.Inconvenience,
-        demand.ServiceImpact,
         demand.Note,
-        demand.ServiceImpact,
       ]
     );
   }
