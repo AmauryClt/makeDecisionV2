589eb7a1758c0d9a04b97d64ff6636666065e77e
CLOT Amaury <amauryclt@gmail.com>
2023-07-13 09:25:27 +0200

    SCSS PopupPage / VotePage V1.2

--- backend/src/models/DemandManager.js
+++ backend/src/models/DemandManager.js
@@ -7,17 +7,14 @@ class DemandManager extends AbstractManager {
 
   add(demand) {
     return this.database.query(
-      `insert into ${this.table}(Title,Deadline,Content,Utility,Benefice,Inconvenience,Complement,Serviceimpact,Note)values (?,?,?,?,?,?,?,?,?)`,
+      `insert into ${this.table}(Title,Deadline,Content,Benefice,Inconvenience,Serviceimpact)values (?,?,?,?,?,?)`,
       [
         demand.Title,
         demand.Deadline,
         demand.Content,
-        demand.Utility,
         demand.Benefice,
         demand.Inconvenience,
-        demand.Complement,
         demand.ServiceImpact,
-        demand.Note,
       ]
     );
   }
