5e017c7f832f0d70f76a35da5bcc4368c9e119fa
Girbau <lgirbau36@gmail.com>
2023-07-19 22:05:30 +0200

    cookies fonctionnel ajout du code à fabien

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import CreatePage from "./components/CreatePage";
@@ -10,6 +11,19 @@ import Profil from "./components/Profil";
 import styles from "./app.module.scss";
 
 function App() {
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  if (user) {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/user/${user}`)
+      .then((response) => response.json())
+      .then((userData) => {
+        setUser(userData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
   return (
     <div className={styles.appForm}>
       <Header />
@@ -17,11 +31,24 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Menu />} />
-          <Route path="/demands/create" element={<CreatePage />} />
-          <Route path="/demands/vote" element={<VotePage />} />
-          <Route path="/demands/valid" element={<ValidPage />} />
+          <Route
+            path="/demands/create"
+            element={<CreatePage setIsUpdated={setIsUpdated} />}
+          />
+          console.info(isUpdated)
+          <Route
+            path="/demands/vote"
+            element={<VotePage isUpdated={isUpdated} />}
+          />
+          <Route
+            path="/demands/valid"
+            element={<ValidPage isUpdated={isUpdated} />}
+          />
           <Route path="/Profil" element={<Profil />} />
-          <Route path="/demands/update/:id" element={<CreatePage />} />
+          <Route
+            path="/demands/update/:id"
+            element={<CreatePage isUpdated={isUpdated} />}
+          />
         </Routes>
       </div>
       <Footer />
