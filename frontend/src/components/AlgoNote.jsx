a8854631c1cc8bace76b59f0e156a81c0b424d5f
alexdubrulle <alexdubrullefagnoni26@hotmail.fr>
2023-07-20 15:07:20 +0200

    algo avec moyenne +texte en fonction de la moyenne

--- frontend/src/components/AlgoNote.jsx
+++ frontend/src/components/AlgoNote.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export default function AlgoNote({ notesByDemand }) {
   const calculateAverageNote = () => {
-    if (notesByDemand.length === 0) return 0;
+    if (notesByDemand.length === 0) return "Soit le premier a voter !";
 
     const totalNotes = notesByDemand.reduce((sum, note) => sum + note.Note, 0);
     const average = totalNotes / notesByDemand.length;
@@ -10,7 +10,29 @@ export default function AlgoNote({ notesByDemand }) {
     return Math.round(average);
   };
 
+  // eslint-disable-next-line consistent-return
+  const getNoteText = (averageNoteValue) => {
+    if (typeof averageNoteValue === "string") {
+      return "Pas d'appréciation moyenne";
+    }
+    if (averageNoteValue <= 1) {
+      return "Profond désaccord";
+    }
+    if (averageNoteValue <= 2) {
+      return "Sceptique";
+    }
+    if (averageNoteValue <= 3) {
+      return "Neutre";
+    }
+    if (averageNoteValue <= 4) {
+      return "Accord";
+    }
+    if (averageNoteValue <= 4.5) {
+      return "Excellente";
+    }
+  };
   const averageNote = calculateAverageNote();
+  const averageNoteText = getNoteText(averageNote);
 
   return (
     <div>
@@ -20,6 +42,7 @@ export default function AlgoNote({ notesByDemand }) {
         </div>
       ))}
       <p>Moyenne des notes pour cette demande : {averageNote}</p>
+      <p>Appréciation moyenne : {averageNoteText}</p>
     </div>
   );
 }
