93f5e176907b873b26eb29f5eed4d41c09d28804
ThomasD26 <thomas@ThomasD>
2023-07-25 12:20:21 +0200

    merge avec la dev

--- frontend/src/components/AdminPage.jsx
+++ frontend/src/components/AdminPage.jsx
@@ -26,101 +26,100 @@ export default function AdminPage() {
 
   return (
     <main>
-      <h1 className={styles.banniere}>Créé un utilisateur</h1>
+      <h1 className={styles.banniere}>Créer un utilisateur</h1>
       <div className={styles.mainHome}>
-        <div className={styles.recalibrage}
-        onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.recalibrage} onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.personne}>
-          <p className={styles.texte}>Nom d'utilisateur :</p>
-          <input
-            {...register("username")}
-            className={styles.champs}
-            type="text"
-            name="username"
-            placeholder="username"
-            required
-          />
-          <p className={styles.texte}>Prénom :</p>
-          <input
-            {...register("Firstname")}
-            className={styles.champs}
-            type="text"
-            name="Firstname"
-            placeholder="Firstname"
-            required
-            onKeyPress={(event) => {
-              const allowedCharacters = /^[^0-9]*$/;
-              if (!allowedCharacters.test(event.key)) {
-                event.preventDefault();
-              }
-            }}
-          />
-          <p className={styles.texte}>Nom de famille :</p>
-          <input
-            {...register("Lastname")}
-            className={styles.champs}
-            type="text"
-            name="Lastname"
-            placeholder="Lastname"
-            required
-            onKeyPress={(event) => {
-              const allowedCharacters = /^[^0-9]*$/;
-              if (!allowedCharacters.test(event.key)) {
-                event.preventDefault();
-              }
-            }}
-          />
+            <p className={styles.texte}>Nom d'utilisateur :</p>
+            <input
+              {...register("username")}
+              className={styles.champs}
+              type="text"
+              name="username"
+              placeholder="username"
+              required
+            />
+            <p className={styles.texte}>Prénom :</p>
+            <input
+              {...register("Firstname")}
+              className={styles.champs}
+              type="text"
+              name="Firstname"
+              placeholder="Firstname"
+              required
+              onKeyPress={(event) => {
+                const allowedCharacters = /^[^0-9]*$/;
+                if (!allowedCharacters.test(event.key)) {
+                  event.preventDefault();
+                }
+              }}
+            />
+            <p className={styles.texte}>Nom de famille :</p>
+            <input
+              {...register("Lastname")}
+              className={styles.champs}
+              type="text"
+              name="Lastname"
+              placeholder="Lastname"
+              required
+              onKeyPress={(event) => {
+                const allowedCharacters = /^[^0-9]*$/;
+                if (!allowedCharacters.test(event.key)) {
+                  event.preventDefault();
+                }
+              }}
+            />
           </div>
           <div className={styles.donnée}>
-          <p className={styles.texte}>Adresse Email :</p>
-          <input
-            {...register("Email")}
-            className={styles.champs}
-            type="text"
-            name="Email"
-            placeholder="Email"
-            required
-          />
-          <p className={styles.texte}>Numéro Mobile :</p>
-          <input
-            {...register("Numeromob")}
-            className={styles.champs}
-            type="number"
-            name="Numeromob"
-            placeholder="Numeromob"
-            required
-            onKeyPress={(event) => {
-              if (!/[0-9]/.test(event.key)) {
-                event.preventDefault();
-              }
-            }}
-            onChange={(event) => {
-              let { value } = event.target;
-              const sanitizedValue = value.replace(/[^0-9]/g, "");
-              if (sanitizedValue.length > 10) {
-                value = sanitizedValue.slice(0, 10);
-              } else {
-                value = sanitizedValue;
-              }
-              event.target.value = value;
-            }}
-          />
-          <p className={styles.texte}>Mot de passe :</p>
-          <input
-            {...register("password")}
-            className={styles.champs}
-            type="text"
-            name="password"
-            placeholder="hashedPassword"
-            required
-          />
+            <p className={styles.texte}>Adresse Email :</p>
+            <input
+              {...register("Email")}
+              className={styles.champs}
+              type="text"
+              name="Email"
+              placeholder="Email"
+              required
+            />
+            <p className={styles.texte}>Numéro Mobile :</p>
+            <input
+              {...register("Numeromob")}
+              className={styles.champs}
+              type="number"
+              name="Numeromob"
+              placeholder="Numeromob"
+              required
+              onKeyPress={(event) => {
+                if (!/[0-9]/.test(event.key)) {
+                  event.preventDefault();
+                }
+              }}
+              onChange={(event) => {
+                let { value } = event.target;
+                const sanitizedValue = value.replace(/[^0-9]/g, "");
+                if (sanitizedValue.length > 10) {
+                  value = sanitizedValue.slice(0, 10);
+                } else {
+                  value = sanitizedValue;
+                }
+                event.target.value = value;
+              }}
+            />
+            <p className={styles.texte}>Mot de passe :</p>
+            <input
+              {...register("password")}
+              className={styles.champs}
+              type="text"
+              name="password"
+              placeholder="hashedPassword"
+              required
+            />
           </div>
-          </div>
-          <div className={styles.btnsubmit}>
-            <button className={styles.Modifier} type="submit">
-              Ajouter un utilisateur
-            </button>
-          </div>
+        </div>
+        <div className={styles.btnsubmit}>
+          <button className={styles.Modifier} type="submit">
+            Ajouter un utilisateur
+          </button>
+        </div>
       </div>
     </main>
   );
